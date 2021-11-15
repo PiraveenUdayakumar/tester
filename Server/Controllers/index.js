f60a8eeb8e734631b86df97adc7b0d35bdc60571
Piraveen Udayakumar <piraveenudayakumar@Piraveens-MacBook-Pro.local>
2021-11-15 17:36:07 -0500

    working

--- Server/Controllers/index.js
+++ Server/Controllers/index.js
@@ -12,12 +12,12 @@ Date: November 8, 2021
 Description: Survey web application that has full CRUD functionality using Express, Node.JS, MongoDB and EJS templating engine.
 */
 
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 
-let DB = require('../config/db');
+let DB = require("../Config/db");
 
 module.exports.displayHomePage = (req, res, next) => {
-    res.render('contents/home', { title: 'Home' });
-}
+  res.render("contents/home", { title: "Home" });
+};
